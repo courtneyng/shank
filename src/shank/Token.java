bdbaf0f8964952bd1fd558f136cf22ef10981f26
courtneyng <courtney.ng81@gmail.com>
2023-02-04 18:18:11 -0500

    public accessors to Token.java

--- src/shank/Token.java
+++ src/shank/Token.java
@@ -5,6 +5,17 @@ public class Token {
         WORD, NUMBER, ENDOFLINE
     }
 
+    private tokenType type;
+    private String value;
+
+    public tokenType getType(){
+        return this.type;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
 
     /**
      * To String Overload
