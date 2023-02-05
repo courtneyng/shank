edcf7cceb3dea12c6cc7641ce9ebdaa40f870370
courtneyng <courtney.ng81@gmail.com>
2023-02-05 13:53:13 -0500

    adjusted switch cases, decimal not completely fixed

--- src/shank/Lexer.java
+++ src/shank/Lexer.java
@@ -9,65 +9,62 @@ public class Lexer {
         char[] expression = inputLine.toCharArray();
         char token;
         String state = "start";
-        boolean hasDecimal = false;
+        boolean hasDecimal = false, segmentCompleted = false;
         StringBuilder expressionLine = new StringBuilder();
 
-        for (char i : expression) {
+        for(char i : expression){
             token = i;
 
-            //Start State Machine
+            //expressionLine.append(token);
+            // State Machine
             switch (state) {
-                case "start":
-                    if (Character.isLetter(token)) { // WORD CASE
-                        expressionLine.append(token);
-                        state = "word";
-                    } else if (Character.isDigit(token)) { // NUMBER CASE
-                        expressionLine.append(token);
-                        state = "number";
-                    } else if (token == '.') {  // DECIMAL CASE
-                        System.out.print(hasDecimal + " ");
-                        expressionLine.append(token);
-                        if (!hasDecimal) { // if hasDecimal is false
+                case "start" -> {
+                    if(token == '.'){
+                        System.out.print(hasDecimal);
+                        if (hasDecimal) throw new Exception("Already has decimal");
+                        else { // if no decimal already
                             hasDecimal = true;
                             state = "decimal";
-                        } else {
-                            System.out.print("did we reach?");
-                            throw new Exception("Already a decimal in number");
                         }
-                    } else if (token == ' ') { // SPACE
+                    }
+                    else if (token == ' ') state = "start";
+                    else if (Character.isLetter(token)) state = "word";
+                    else if (Character.isDigit(token)) state = "number";
+                    else{
+                        System.out.print("[" + token + "]");
+                        throw new Exception("The token in brackets is not accepted");
+                    }
+                }
+                case "word" -> {
+                    if (Character.isLetterOrDigit(token)) {
                         expressionLine.append(token);
-                        state = "start";
-                    } else { // "ANYTHING ELSE"
-                        System.out.print("[" + token + "] ");
-                        throw new Exception("The token in the brackets is not accepted");
-                    }
-                case "word":
-                    if (Character.isLetter(token) || Character.isDigit(token)) {
-                        //System.out.print("!");
                         state = "word";
-                    } else{
+                    } else {
                         state = "start";
                     }
-                case "decimal":
+                }
+                case "decimal" -> {
                     if (Character.isDigit(token)) {
+                        expressionLine.append(token + "!");
                         state = "decimal";
                     } else {
                         state = "start";
                     }
-                case "number":
+                }
+                case "number" -> {
                     if (Character.isDigit(token)) {
-                        expressionLine.append(token);
-                        state = "number";
+                        expressionLine.append(token + "?");
+                        state = "decimal";
                     } else if (token == '.') {
-                        //expressionLine.append(token);
+                        expressionLine.append(token);
                         hasDecimal = true;
                         state = "decimal";
                     } else {
                         state = "start";
                     }
+                }
             }
-
-        } // END FOR LOOP
+        }
         expressionLine.append(" " + Token.tokenType.ENDOFLINE);
         System.out.println(expressionLine);
     }
