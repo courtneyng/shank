e540b721d148476388a299e7915e80c3ca437166
courtneyng <courtney.ng81@gmail.com>
2023-02-17 10:02:09 -0500

    edit skeleton of parser and node classes

--- src/shank/MathOpNode.java
+++ src/shank/MathOpNode.java
@@ -1,4 +1,8 @@
 package shank;
 
 public class MathOpNode extends Node{
+    //make a constructor that takes token type that converts to enum here?
+    enum MathOp{
+        ADD, SUBTRACT, MULTIPLY, DIVIDE
+    }
 }
--- src/shank/Parser.java
+++ src/shank/Parser.java
@@ -1,4 +1,34 @@
 package shank;
 
 public class Parser {
+    public void Parse(){
+
+    }
+
+
+    //TERM { (plus or minus) TERM}
+    public void expression(){
+
+    }
+
+    //FACTOR { (times or divide or mod) FACTOR}
+    public void term(){
+
+    }
+    //{-} number or lparen EXPRESSION rparen
+    public void factor(){
+
+    }
+    private void matchAndRemove(){
+
+    }
+
+    private void expectsEOL(){
+
+    }
+
+    private void peek(){
+
+    }
+
 }
