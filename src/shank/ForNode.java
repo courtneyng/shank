30eafdc57988e0d6a9ed3bc8a06558e755148746
courtneyng <courtney.ng81@gmail.com>
2023-03-25 17:11:45 -0400

    update ForNode.java

--- src/shank/ForNode.java
+++ src/shank/ForNode.java
@@ -1,4 +1,33 @@
 package shank;
 
-public class ForNode {
+import java.util.ArrayList;
+
+public class ForNode extends StatementNode{
+
+    private ArrayList<StatementNode> statements;
+    private Node from;
+    private Node to;
+    private VariableReferenceNode variable;
+
+    /**
+     * Empty Constructor
+     */
+    public ForNode(){}
+
+    public ForNode(Node from, Node to, ArrayList<StatementNode> statements, VariableReferenceNode variable) {
+        this.from = from;
+        this.to = to;
+        this.statements = statements;
+        this.variable = variable;
+    }
+
+    @Override
+    public String toString() {
+        String fullStatement = "From: " + from.toString() + " To: " + to.toString() + " Variable " +
+                variable.toString() + " Statements: " + statements.toString();
+        for(int i=0; i<statements.size(); i++){
+            fullStatement = fullStatement + "\n" + statements.get(i).toString();
+        }
+        return fullStatement;
+    }
 }
