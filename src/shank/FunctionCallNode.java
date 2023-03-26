3e172f813e816ed1ca6993822da0f0f23b4bf709
courtneyng <courtney.ng81@gmail.com>
2023-03-26 13:02:12 -0400

    update FunctionCallNode.java

--- src/shank/FunctionCallNode.java
+++ src/shank/FunctionCallNode.java
@@ -1,4 +1,26 @@
 package shank;
 
-public class FunctionCallNode{
+import java.util.*;
+
+public class FunctionCallNode extends StatementNode{
+
+    private ArrayList<ParameterNode> parameters;
+    private String functionName;
+
+    public FunctionCallNode(){}
+
+    public FunctionCallNode(String functionName, ArrayList<ParameterNode> parameters){
+        this.functionName = functionName;
+        this.parameters = parameters;
+    }
+
+    @Override
+    public String toString() {
+        String str = "\n[FunctionCallNode] Function name: " + functionName + "\n Parameters: \n";
+        for(int i=0; i<parameters.size(); i++){
+            str += parameters.get(i).toString() + "\n";
+        }
+        str += ")";
+        return str;
+    }
 }
