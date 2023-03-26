a894832f9ba41863c0eb1267b8633657ba190a80
courtneyng <courtney.ng81@gmail.com>
2023-03-26 11:03:14 -0400

    update IfNode.java

--- src/shank/IfNode.java
+++ src/shank/IfNode.java
@@ -1,8 +1,75 @@
 package shank;
 
-public class IfNode extends Node{
+import java.util.*;
+
+public class IfNode extends StatementNode{
+    private ArrayList<StatementNode> statements;
+    private BooleanCompareNode condition;
+    private IfNode next;
+
+    /**
+     * empty constructor
+     */
+    public IfNode(){}
+
+    /**
+     *  else statement - no conditions or next
+     *  else{ body }
+     * @param statements
+     */
+    public IfNode(ArrayList<StatementNode> statements){
+        this.statements = statements;
+        this.condition = null;
+        this.next = null;
+    }
+
+    /**
+     * Condition for next if or else-if
+     * @param statements
+     * @param condition
+     * @param next
+     */
+    public IfNode(ArrayList<StatementNode> statements, BooleanCompareNode condition, IfNode next){
+        this.statements = statements;
+        this.condition = condition;
+        this.next = next;
+    }
+
+    /**
+     * end else-if
+     * @param statements
+     * @param condition
+     */
+    public IfNode(ArrayList<StatementNode> statements, BooleanCompareNode condition){
+        this.statements = statements;
+        this.condition = condition;
+        this.next = null;
+    }
+
+    public ArrayList<StatementNode> getStatements(){
+        return statements;
+    }
+
+    public BooleanCompareNode getCondition() {
+        return condition;
+    }
+
+    public IfNode getNext(){
+        return next;
+    }
+
     @Override
     public String toString() {
-        return null;
+        String str;
+
+        if(this.condition == null) str = "\n IfNode else statements: ";
+        else str = "\n IfNode condition: (" + condition.toString() + ") \n statements:";
+
+        for(int i=0; i<statements.size(); i++){
+            str = str + "\n " + statements.get(i).toString();
+        }
+
+        if(this.next == null) return str;
+        else return str + next.toString();
     }
 }
