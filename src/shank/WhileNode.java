debaf7c073e249d525525bd84cdc38cbe666ba90
courtneyng <courtney.ng81@gmail.com>
2023-03-26 12:06:36 -0400

    update WhileNode.java

--- src/shank/WhileNode.java
+++ src/shank/WhileNode.java
@@ -1,8 +1,26 @@
 package shank;
 
-public class WhileNode extends Node{
+import java.util.*;
+
+public class WhileNode extends StatementNode{
+    private ArrayList<StatementNode> statements;
+    private BooleanCompareNode condition;
+
+    public WhileNode(){}
+
+    public WhileNode(ArrayList<StatementNode> statements, BooleanCompareNode condition){
+        this.statements = statements;
+        this.condition = condition;
+    }
+
     @Override
     public String toString() {
-        return null;
+        String str = "[While] Condition: (" + condition.toString() + ") Statements:";
+        for(int i=0; i<statements.size(); i++){
+            str = str + "\n " + statements.get(i).toString();
+        }
+        str += ")";
+
+        return str;
     }
 }
