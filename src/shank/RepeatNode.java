70a1e2a0a497ce1c45fb29ec3d4fa73712cca916
courtneyng <courtney.ng81@gmail.com>
2023-03-26 11:08:20 -0400

    update RepeatNode.java

--- src/shank/RepeatNode.java
+++ src/shank/RepeatNode.java
@@ -1,4 +1,34 @@
 package shank;
 
-public class RepeatNode {
+import java.util.*;
+
+public class RepeatNode extends StatementNode{
+    private ArrayList<StatementNode> statements;
+    private BooleanCompareNode condition;
+
+    public RepeatNode(){}
+
+    public RepeatNode(ArrayList<StatementNode> statements, BooleanCompareNode condition){
+        this.statements = statements;
+        this.condition = condition;
+    }
+
+    public ArrayList<StatementNode> getStatements(){
+        return statements;
+    }
+
+    public BooleanCompareNode getCondition() {
+        return condition;
+    }
+
+    @Override
+    public String toString() {
+        String str = "[RepeatNode] Condition: (" + condition.toString() + ") \nStatements: ";
+        for(int i=0; i< statements.size(); i++){
+            str = str + "\n " + statements.get(i).toString();
+        }
+        str += ")";
+
+        return str;
+    }
 }
