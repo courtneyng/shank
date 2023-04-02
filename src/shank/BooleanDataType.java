45e781bf1799fc83098ba7e6aca172d2a01c96cd
courtneyng <courtney.ng81@gmail.com>
2023-04-02 11:27:06 -0400

    added body to BooleanDataType.java

--- src/shank/BooleanDataType.java
+++ src/shank/BooleanDataType.java
@@ -1,4 +1,22 @@
 package shank;
 
-public class BooleanDataType {
+public class BooleanDataType extends InterpreterDataType{
+
+    private boolean value;
+
+    public BooleanDataType(){}
+
+    public BooleanDataType(boolean value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BooleanDataType: [" + value + "] ";
+    }
+
+    @Override
+    public void fromString(String input) {
+        value = Boolean.parseBoolean(input);
+    }
 }
