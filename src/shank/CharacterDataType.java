6356e152a490604bcdac1c684c05cb0fa4137135
courtneyng <courtney.ng81@gmail.com>
2023-04-02 17:12:44 -0400

    added body to CharacterDataType.java

--- src/shank/CharacterDataType.java
+++ src/shank/CharacterDataType.java
@@ -1,4 +1,22 @@
 package shank;
 
-public class CharacterDataType {
+public class CharacterDataType extends InterpreterDataType{
+
+    private char value;
+
+    public CharacterDataType(){}
+
+    public CharacterDataType(char value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "CharacterDataType [" + value + "] ";
+    }
+
+    @Override
+    public void fromString(String input) {
+        value = input.charAt(0);
+    }
 }
