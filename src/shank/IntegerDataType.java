653d4513aa2de6aed2cc9e7d4cda53a6f85ae112
courtneyng <courtney.ng81@gmail.com>
2023-04-02 11:24:51 -0400

    init BuiltInRead.java, added body to IntegerDataType.java and InterpreterDataType.java

--- src/shank/IntegerDataType.java
+++ src/shank/IntegerDataType.java
@@ -1,4 +1,22 @@
 package shank;
 
-public class IntegerDataType {
+public class IntegerDataType extends InterpreterDataType{
+
+    private int value;
+
+    public IntegerDataType(){}
+
+    public IntegerDataType(int value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "IntegerDataType: [" + value + "] ";
+    }
+
+    @Override
+    public void fromString(String input) {
+        value = Integer.parseInt(input);
+    }
 }
