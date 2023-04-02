99ee0c79b6bbdc073717acb49059e8b8cc8d41e5
courtneyng <courtney.ng81@gmail.com>
2023-04-02 11:28:48 -0400

    added body to RealDataType.java

--- src/shank/RealDataType.java
+++ src/shank/RealDataType.java
@@ -1,4 +1,21 @@
 package shank;
 
-public class RealDataType {
+public class RealDataType extends InterpreterDataType{
+
+    private float value;
+
+    public RealDataType(){}
+
+    public RealDataType(float value){
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "RealDataType: [" + value + "] ";
+    }
+
+    @Override
+    public void fromString(String input) {
+        value = Float.parseFloat(input);
+    }
 }
