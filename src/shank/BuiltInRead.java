5325f49764668e4dcac6a34e74a2c49260f6840e
courtneyng <courtney.ng81@gmail.com>
2023-04-02 22:16:43 -0400

    add execute to built in read

--- src/shank/BuiltInRead.java
+++ src/shank/BuiltInRead.java
@@ -14,9 +14,37 @@ public class BuiltInRead extends FunctionNode {
         super(name, variables);
     }
 
-    IntegerDataType execute(){
+    public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
         Scanner scan = new Scanner(System.in);
-        return null;
+        for(InterpreterDataType type : data){
+            if(type instanceof IntegerDataType){
+                IntegerDataType integer = (IntegerDataType) type;
+                System.out.println("Enter an int value: ");
+                integer.setValue(scan.nextInt());
+                type = integer;
+            } else if (type instanceof RealDataType) {
+                RealDataType real = (RealDataType) type;
+                System.out.println("Enter an real (float) value: ");
+                real.setValue(scan.nextFloat());
+                type = real;
+            } else if (type instanceof StringDataType) {
+                StringDataType str = (StringDataType) type;
+                System.out.println("Enter an int value: ");
+                str.setValue(scan.nextLine());
+                type = str;
+            } else if (type instanceof CharacterDataType) {
+                CharacterDataType character = (CharacterDataType) type;
+                System.out.println("Enter an int value: ");
+                character.setValue(scan.nextLine().charAt(0));
+                type = character;
+            } else if (type instanceof BooleanDataType) {
+                BooleanDataType bool = (BooleanDataType) type;
+                System.out.println("Enter an int value: ");
+                bool.setValue(scan.nextBoolean());
+                type = bool;
+            } else throw new SyntaxErrorException("[BuiltInRead: execute] Expected: changeable argument");
+        }
+        scan.close();
     }
 
 
