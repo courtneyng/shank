4f391e6f735ee1203a6cf9172aacc2bc0ec9cc39
courtneyng <courtney.ng81@gmail.com>
2023-04-02 22:01:01 -0400

    update execute to built in left

--- src/shank/BuiltInLeft.java
+++ src/shank/BuiltInLeft.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class BuiltInLeft extends FunctionNode{
     /**
-     * Default Constructor from functionnode
+     * Default Constructor from function node
      * @param name - name
      * @param variables - variables
      */
@@ -13,18 +13,18 @@ public class BuiltInLeft extends FunctionNode{
     }
 
     public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
-        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable() == true){
+        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable()){
            StringDataType strData = (StringDataType) data.get(0);
            IntegerDataType dataLength = (IntegerDataType) data.get(1);
            StringDataType string = (StringDataType) data.get(2);
 
            int length = dataLength.getValue();
            String str = strData.getValue();
-           String finalStr = "";
+           StringBuilder finalStr = new StringBuilder();
            for(int i=0; i<length; i++){
-               finalStr += str.charAt(i);
+               finalStr.append(str.charAt(i));
            }
-           string.setValue(finalStr);
+           string.setValue(finalStr.toString());
            data.set(2, string);
         }
         else throw new SyntaxErrorException("[BuiltInLeft: execute] Does not contain the correct arguments for function");
