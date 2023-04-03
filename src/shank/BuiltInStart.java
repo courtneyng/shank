286194dda6ac0b65ab3e7f9a6f4c5198dd2add7e
courtneyng <courtney.ng81@gmail.com>
2023-04-02 22:31:46 -0400

    update execute to built in start

--- src/shank/BuiltInStart.java
+++ src/shank/BuiltInStart.java
@@ -1,4 +1,23 @@
 package shank;
 
-public class BuiltInStart {
+import java.util.ArrayList;
+
+public class BuiltInStart extends FunctionNode{
+    /**
+     * Default constructor
+     * @param name - name
+     * @param variables - variables
+     */
+    public BuiltInStart(String name, ArrayList<VariableNode> variables) {
+        super(name, variables);
+    }
+
+    public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
+        if(data.get(0) instanceof ArrayDataType && ((ArrayDataType) data.get(0)).isChangeable()){
+            ArrayDataType arrVal = (ArrayDataType) data.get(0);
+            IntegerDataType startIndex = new IntegerDataType(arrVal.getStartIndex(), false);
+            data.set(0, startIndex);
+        }
+        else throw new SyntaxErrorException("[BuiltInStart: execute] Does not contain the correct arguments for function");
+    }
 }
