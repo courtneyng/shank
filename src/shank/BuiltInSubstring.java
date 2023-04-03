b1a3f0c0935357a94bb91e2f9db5f5f422df5434
courtneyng <courtney.ng81@gmail.com>
2023-04-02 21:58:45 -0400

    update execute to built in substring

--- src/shank/BuiltInSubstring.java
+++ src/shank/BuiltInSubstring.java
@@ -13,7 +13,7 @@ public class BuiltInSubstring extends FunctionNode{
     }
 
     public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
-        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable() == true){
+        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable()){
             StringDataType strData = (StringDataType) data.get(0);
             IntegerDataType indexVal = (IntegerDataType) data.get(1);
             IntegerDataType lengthVal = (IntegerDataType) data.get(2);
@@ -23,12 +23,12 @@ public class BuiltInSubstring extends FunctionNode{
             int length = lengthVal.getValue();
 
             String string = strData.getValue();
-            String finalStr = "";
+            StringBuilder finalStr = new StringBuilder();
 
             for(int i=0; i<index+length; i++){
-                finalStr += string.charAt(i);
+                finalStr.append(string.charAt(i));
             }
-            str.setValue(finalStr);
+            str.setValue(finalStr.toString());
             data.set(3, str);
         }
         else throw new SyntaxErrorException("[BuiltInSubstring: execute] Does not contain the correct arguments for function");
