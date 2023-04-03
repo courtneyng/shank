79a084d6ef6c681c1dd3e493d00888458207f6c0
courtneyng <courtney.ng81@gmail.com>
2023-04-02 22:17:39 -0400

    update execute to built in read

--- src/shank/BuiltInRead.java
+++ src/shank/BuiltInRead.java
@@ -17,32 +17,30 @@ public class BuiltInRead extends FunctionNode {
     public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
         Scanner scan = new Scanner(System.in);
         for(InterpreterDataType type : data){
-            if(type instanceof IntegerDataType){
-                IntegerDataType integer = (IntegerDataType) type;
-                System.out.println("Enter an int value: ");
-                integer.setValue(scan.nextInt());
-                type = integer;
-            } else if (type instanceof RealDataType) {
-                RealDataType real = (RealDataType) type;
-                System.out.println("Enter an real (float) value: ");
-                real.setValue(scan.nextFloat());
-                type = real;
-            } else if (type instanceof StringDataType) {
-                StringDataType str = (StringDataType) type;
-                System.out.println("Enter an int value: ");
-                str.setValue(scan.nextLine());
-                type = str;
-            } else if (type instanceof CharacterDataType) {
-                CharacterDataType character = (CharacterDataType) type;
-                System.out.println("Enter an int value: ");
-                character.setValue(scan.nextLine().charAt(0));
-                type = character;
-            } else if (type instanceof BooleanDataType) {
-                BooleanDataType bool = (BooleanDataType) type;
-                System.out.println("Enter an int value: ");
-                bool.setValue(scan.nextBoolean());
-                type = bool;
-            } else throw new SyntaxErrorException("[BuiltInRead: execute] Expected: changeable argument");
+            switch (type) {
+                case IntegerDataType integer -> {
+                    System.out.println("Enter an int value: ");
+                    integer.setValue(scan.nextInt());
+                }
+                case RealDataType real -> {
+                    System.out.println("Enter an real (float) value: ");
+                    real.setValue(scan.nextFloat());
+                }
+                case StringDataType str -> {
+                    System.out.println("Enter an int value: ");
+                    str.setValue(scan.nextLine());
+                }
+                case CharacterDataType character -> {
+                    System.out.println("Enter an int value: ");
+                    character.setValue(scan.nextLine().charAt(0));
+                }
+                case BooleanDataType bool -> {
+                    System.out.println("Enter an int value: ");
+                    bool.setValue(scan.nextBoolean());
+                }
+                case null, default ->
+                        throw new SyntaxErrorException("[BuiltInRead: execute] Expected: changeable argument");
+            }
         }
         scan.close();
     }
