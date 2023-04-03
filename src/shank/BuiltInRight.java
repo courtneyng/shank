b8d96e3287deb7225f543c50280369b7d3205e38
courtneyng <courtney.ng81@gmail.com>
2023-04-02 22:02:46 -0400

    update execute to built in right

--- src/shank/BuiltInRight.java
+++ src/shank/BuiltInRight.java
@@ -13,18 +13,18 @@ public class BuiltInRight extends FunctionNode{
     }
 
     public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
-        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable() == true){
+        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable()){
             StringDataType strData = (StringDataType) data.get(0);
             IntegerDataType dataLength = (IntegerDataType) data.get(1);
             StringDataType string = (StringDataType) data.get(2);
 
             int length = dataLength.getValue();
             String str = strData.getValue();
-            String finalStr = "";
-            for(int i=0; i<str.length(); i++){
-                finalStr += str.charAt(i);
+            StringBuilder finalStr = new StringBuilder();
+            for(int i=length; i<str.length(); i++){
+                finalStr.append(str.charAt(i));
             }
-            string.setValue(finalStr);
+            string.setValue(finalStr.toString());
             data.set(2, string);
         }
         else throw new SyntaxErrorException("[BuiltInRight: execute] Does not contain the correct arguments for function");
