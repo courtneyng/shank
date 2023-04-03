0e217e883a84b8f8e15407bc5e2b6cea203c396d
courtneyng <courtney.ng81@gmail.com>
2023-04-02 22:24:27 -0400

    update execute to built in sqrt

--- src/shank/BuiltInSquareRoot.java
+++ src/shank/BuiltInSquareRoot.java
@@ -1,4 +1,26 @@
 package shank;
 
-public class BuiltInSquareRoot {
+import java.util.ArrayList;
+
+public class BuiltInSquareRoot extends FunctionNode{
+    /**
+     * Default constructor from function node
+     * @param name - name
+     * @param variables - variables
+     */
+    public BuiltInSquareRoot(String name, ArrayList<VariableNode> variables) {
+        super(name, variables);
+    }
+
+    public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
+        if(data.get(0) instanceof StringDataType && ((RealDataType) data.get(0)).isChangeable()){
+            RealDataType realVal = (RealDataType) data.get(0);
+            RealDataType endVal = (RealDataType)  data.get(1);
+
+            float real = realVal.getValue();
+            float result = (float)Math.sqrt(real);
+            endVal.setValue(result);
+            data.set(1, endVal);
+        }else throw new SyntaxErrorException("[BuiltInSquareRoot: execute] Does not contain the correct arguments for function");
+    }
 }
