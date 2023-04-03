671153f8571cbc9ba0fac07b525b56829f83cb3e
courtneyng <courtney.ng81@gmail.com>
2023-04-02 21:59:47 -0400

    update execute to built in getrand

--- src/shank/BuiltInGetRandom.java
+++ src/shank/BuiltInGetRandom.java
@@ -8,7 +8,7 @@ public class BuiltInGetRandom extends FunctionNode{
     }
 
     public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
-        if(data.get(0) instanceof IntegerDataType && data.size() == 1 && ((IntegerDataType) data.get(0)).isChangeable() == true){
+        if(data.get(0) instanceof IntegerDataType && data.size() == 1 && ((IntegerDataType) data.get(0)).isChangeable()){
             Random rand = new Random();
             IntegerDataType intData = (IntegerDataType) data.get(0);
             int randNum = rand.nextInt(101); // number between 0 and 100
