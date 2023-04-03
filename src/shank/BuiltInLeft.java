427fcaa93989c4639dd81af724858018fc7490dc
courtneyng <courtney.ng81@gmail.com>
2023-04-02 21:50:05 -0400

    add execute to built in left

--- src/shank/BuiltInLeft.java
+++ src/shank/BuiltInLeft.java
@@ -1,4 +1,32 @@
 package shank;
 
-public class BuiltInLeft {
+import java.util.ArrayList;
+
+public class BuiltInLeft extends FunctionNode{
+    /**
+     * Default Constructor from functionnode
+     * @param name - name
+     * @param variables - variables
+     */
+    public BuiltInLeft(String name, ArrayList<VariableNode> variables) {
+        super(name, variables);
+    }
+
+    public void execute(ArrayList<InterpreterDataType> data) throws SyntaxErrorException{
+        if(data.get(0) instanceof StringDataType && ((StringDataType) data.get(0)).isChangeable() == true){
+           StringDataType strData = (StringDataType) data.get(0);
+           IntegerDataType dataLength = (IntegerDataType) data.get(1);
+           StringDataType string = (StringDataType) data.get(2);
+
+           int length = dataLength.getValue();
+           String str = strData.getValue();
+           String finalStr = "";
+           for(int i=0; i<length; i++){
+               finalStr += str.charAt(i);
+           }
+           string.setValue(finalStr);
+           data.set(2, string);
+        }
+        else throw new SyntaxErrorException("[BuiltInLeft: execute] Does not contain the correct arguments for function");
+    }
 }
